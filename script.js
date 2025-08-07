0b76ba5cc9e759e77b313e8654b94f25eb03524d
yuriown <yurijogaizi@gmail.com>
2025-08-07 15:51:46 -0300

    atualizar commit

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.getElementById("showButton").addEventListener("click", function() {
   
     // Defina a URL da imagem e a frase que deseja exibir
     image.src = 'https://museudememes.com.br/wp-content/uploads/2022/05/bf3d6d63d767c049a5073f56581e8275.jpg'; // Link da imagem
-    phrase.textContent = "mucha sdds :(";
+    phrase.textContent = "mi disculpa, jaque :(";
   
     // Adiciona a classe "show" para exibir a imagem e a frase com o efeito de delay
     image.classList.add("show");
