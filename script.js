8fbc1b684ab0f2c4cfe79cb924c957bc60349d0d
yuriown <yurijogaizi@gmail.com>
2025-08-11 17:38:37 -0300

    oi

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ document.getElementById("showButton").addEventListener("click", function() {
     const phrase = document.getElementById("phrase");
   
     // Defina a URL da imagem e a frase que deseja exibir
-    image.src = 'https://museudememes.com.br/wp-content/uploads/2022/05/bf3d6d63d767c049a5073f56581e8275.jpg'; // Link da imagem
-    phrase.textContent = "mi disculpa, jaque :(";
+    image.src = 'https://museudememes.com.br/wp-content/uploads/2022/05/bf3d6d63d767c049a5073f56581e8275.jpg'; // Link da imagem https://museudememes.com.br/wp-content/uploads/2022/05/bf3d6d63d767c049a5073f56581e8275.jpg
+    phrase.textContent = "oi jaque :)";
   
     // Adiciona a classe "show" para exibir a imagem e a frase com o efeito de delay
     image.classList.add("show");
