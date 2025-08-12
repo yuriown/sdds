13e3ff07bc8af1b9dcd203794f5f6683b2f2680f
yuriown <yurijogaizi@gmail.com>
2025-08-12 15:15:35 -0300

    vlw

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.getElementById("showButton").addEventListener("click", function() {
   
     // Defina a URL da imagem e a frase que deseja exibir
     image.src = 'https://i.pinimg.com/736x/35/10/c5/3510c5f4c7b34b3933548d49f8c4091e.jpg'; // Link da imagem https://museudememes.com.br/wp-content/uploads/2022/05/bf3d6d63d767c049a5073f56581e8275.jpg
-    phrase.textContent = "oi jaque :)";
+    phrase.textContent = "é nois :)";
   
     // Adiciona a classe "show" para exibir a imagem e a frase com o efeito de delay
     image.classList.add("show");
